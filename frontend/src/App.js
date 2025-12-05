ca3a3badda18ba0903a4c0cfeee9c088b08faa6f
LiliGilina <lildimgil@gmail.com>
2025-12-05 11:45:45 +0200

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>Lili's cooking</h1>
+         <h2>welcome</h2>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
